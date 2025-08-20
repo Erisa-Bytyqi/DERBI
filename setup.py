3139c8678e729ed240d329caf410d97ed783023f
Erisa-Bytyqi <erisabytyqi20@gmail.com>
2025-08-20 19:22:42 +0200

    update setup

--- setup.py
+++ setup.py
@@ -7,11 +7,18 @@ desc = '''
 
 setuptools.setup(
     name='DERBI',
-    version="1.1",
+    version="1.2",
     author='Max Schmaltz',
     authors_email='schmaltzmax@gmail.com',
     description=desc,
     packages=['DERBI'],
-    package_data={'DERBI': ['Router.json', 'meta/*', 'meta/automata/*', 'meta/lexicons/*']},
+    package_data={'DERBI': ['Router.json', 'meta/*',
+                            'meta/automata/*', 'meta/lexicons/*']},
     include_package_data=True,
+    install_requires=[
+        "nltk==3.7",
+        "numpy==1.23.5",
+        "spacy==3.5.1",
+        "compound-split==1.0.2",
+    ],
 )
